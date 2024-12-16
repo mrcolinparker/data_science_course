59f78c746a913736765658a769d98c4310f4de79
mrcolinparker <mrcolinparker@yahoo.com>
2024-12-16 21:10:09 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,15 @@
+#changing data types
+#the data to change
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+#converting num1 to an int, num2 to a float
+#num3 to a string, num4 to an int
+
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
+
