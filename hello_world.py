7370971736853c9bd190d11c36913caaa4e58b3e
mrcolinparker <mrcolinparker@yahoo.com>
2024-12-16 20:59:33 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#user to enter name
+name = input("Enter your name:")
+print(name)
+
+#user to input age
+age = int(input("Enter your age:"))
+print(age)
+
+#output Hello World!
+print("Hello World!")
+
